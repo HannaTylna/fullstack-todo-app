47d396664d6a61268039ed9fd99b3cb4af709954
Hanna <90796659+HannaTylna@users.noreply.github.com>
2022-09-23 23:56:08 +0200

    add todo router & dotenv

--- packages/backend/src/app.ts
+++ packages/backend/src/app.ts
@@ -1,16 +1,19 @@
 import express, { Application, json, Request, Response } from "express";
 import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app: Application = express();
+const todoRouter = require("../routers/todos");
 app.use(cors());
 app.use(json());
 
-const port: number = parseInt(process.env.SERVER_PORT || "3001");
+app.use("/todos", todoRouter)
 
-app.get("/hello", (req: Request, res: Response) => {
-  res.send("Hello, World!");
-});
+const port: number = parseInt(process.env.SERVER_PORT || "3001");
 
 app.listen(port, function() {
   console.log(`App is listening on port ${port} !`);
 });
+
