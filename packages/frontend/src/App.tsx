4cdd3f60fa30d56609b75775bab1bcf9b7c86833
Hanna <90796659+HannaTylna@users.noreply.github.com>
2022-09-23 23:53:57 +0200

    add url axios

--- packages/frontend/src/App.tsx
+++ packages/frontend/src/App.tsx
@@ -2,6 +2,9 @@ import React from "react";
 import "./App.css";
 import Navbar from "./components/Navbar";
 import TodoForm from "./components/TodoForm";
+import axios from "axios";
+
+axios.defaults.baseURL = process.env.REACT_APP_TODO_API || "http://localhost:3001";
 
 function App() {
   return (
